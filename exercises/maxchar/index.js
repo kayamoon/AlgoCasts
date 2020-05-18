bcb556733e97f26d6727a30dd955eaa0877dfef5
Firstname Lastname <chou@simmons.edu>
2020-05-17 22:05:12 -0400

    MaxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let charMap = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let char of str) { //use for of loop for string or array
+        if (charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    }
+    
+    for (let char in charMap) { //use for in for object //char is assigned the keys of the object
+        if (charMap[char] > max) {
+            max = charMap[char];
+            maxChar = char.toString();
+        }
+    }
+
+    return maxChar;
+
+}
 
 module.exports = maxChar;
