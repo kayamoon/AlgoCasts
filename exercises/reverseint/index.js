f0313740d8bd91f8646f1e63fd2fe0fa63559c88
Firstname Lastname <chou@simmons.edu>
2020-05-17 22:05:41 -0400

    Reverse Integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,29 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const reverse = n.toString().split('').reverse().join('');
+    return parseInt(reverse) * Math.sign(n); //Math.sign() returns 1 if sign is positive and -1 if sign is negative
+}
+
+console.log(reverseInt(-400));
 
 module.exports = reverseInt;
+
+
+/* MY FIRST SOLUTION (confusing/too long)
+function reverseInt(n) {
+     if(Math.sign(n) === 1 || n == 0){
+        return parseInt(n.toString().split('').reverse().join('')) 
+    } else {
+        return parseInt((n*-1).toString().split('').reverse().join(''))*-1; 
+    }
+}
+*/
+
+/* BEST SOLUTION
+function reverseInt(n) {
+    const reverse = n.toString().split('').reverse().join('');
+    return parseInt(reverse) * Math.sign(n);
+}
+*/
