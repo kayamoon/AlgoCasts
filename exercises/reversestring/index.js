6f09df8cb2cdd5ab45e64da6f21d9b54994cda7d
Firstname Lastname <chou@simmons.edu>
2020-05-17 22:05:54 -0400

    Reverse String

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,34 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let reverseStr = '';
+    for (let character of str){ //new syntax like a for each loop
+        reverseStr = character + reverseStr; //adds character to the start of the reverseStr string
+    }
+    return reverseStr;
+}
 
 module.exports = reverse;
+
+/*
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+*/
+
+/*
+function reverse(str) {
+    let reverseStr = '';
+    for (let character of str){ //new syntax like a for each loop
+        reverseStr = character + reverseStr; //adds character to the start of the reverseStr string
+    }
+    return reverseStr;
+}
+*/
+
+/*
+function reverse(str) {
+    return str.split('').reduce((reverseStr, char) => char + reverseStr, '');
+}
+*/
