7dd64ebcf764fb4699f3052efe3ce6f982239114
Firstname Lastname <chou@simmons.edu>
2020-05-19 17:20:23 -0400

    Fibonacci and memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,54 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+
+//what can you do to make the run time better?
+//memoization:
+//record the arguments of each function, run the function, and then store the results of the function call
+//so if that function gets called again with the same arguments, it will instead return the previous computed result
+
+
+function memoize(fn){ //returns a function, shortens runtime A LOT
+    const cache = {};
+    return function(...args){ ///...args assigns all args as an array and passes it in, ES6 syntax
+        if(cache[args]){
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args); //fn is the slowFib function, apply helper must be used b/c passing in an array of elements
+        cache[args] = result;
+        return result;
+    }
+}
+
+function fib(n) { //memoization
+    if(n < 2){
+        return n;
+    }
+
+    return (fib(n-1) + fib(n-2)); //these function calls are a reference to the fib below (the memoized version fib)
+}
+
+fib = memoize(fib); //how you would memoize a function, reassign fib to the memoized version of fib
 
 module.exports = fib;
+
+/* Iterative solution //linear run time
+function fib(n) {
+    const arr = [0, 1];
+    for(let i = 1; i < n; i++){
+        arr.push(arr[i-1] + arr[i]);
+    }
+    return arr[n];
+}
+*/
+
+/* Recursive solution
+function fib(n) { //recursive solution, exponential run time
+    if(n < 2){
+        return n;
+    }
+
+    return (fib(n-1) + fib(n-2));
+}
+*/
