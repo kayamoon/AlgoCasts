16ae34e933f3d781d9b6c4e5973dd0a80f4c7cd7
Firstname Lastname <chou@simmons.edu>
2020-05-19 18:07:08 -0400

    Queue from stacks

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,39 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+    constructor(){
+        this.stackA = new Stack();
+        this.stackB = new Stack();
+    }
+
+    add(elem){
+        this.stackA.push(elem);
+    }
+
+    remove(){
+        while(this.stackA.peek()){
+            this.stackB.push(this.stackA.pop());
+        }
+        const top = this.stackB.pop();
+
+        while(this.stackB.peek()){
+            this.stackA.push(this.stackB.pop());
+        }
+        return top;
+    }
+
+    peek(){
+        while(this.stackA.peek()){
+            this.stackB.push(this.stackA.pop());
+        }
+        const top = this.stackB.peek();
+        
+        while(this.stackB.peek()){
+            this.stackA.push(this.stackB.pop());
+        }
+        return top;
+    }
+}
 
 module.exports = Queue;
